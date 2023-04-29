30bcdaf0c78efc3540b2b1c16ae5c1dcad22870d
omar-tolan <omar.tolan@gmail.com>
2023-04-29 13:13:24 +0300

    Set up basic chat functionality

--- src/server.js
+++ src/server.js
@@ -1,7 +1,39 @@
 const express = require('express')
+const path = require("path")
+const http = require('http')
+const socketio = require("socket.io")
+const Filter = require("bad-words")
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-app.listen(3000, () => {
-    console.log("Server up on port 3000")
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, "../public")))
+
+
+io.on('connection', (socket) => {
+    socket.emit("message", "Welcome!")
+    socket.broadcast.emit("message", "A new user just joined!")
+    socket.on("newMessage", (message, callback) => {
+        const filter = new Filter()
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed!')
+        }
+        io.emit("message", message)
+        callback()
+    })
+    socket.on("location", (position, callback) => {
+        socket.broadcast.emit("message", `https://google.com/maps?q=${position.longitude},${position.latitude}`)
+        callback()
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has disconnected!")
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server up on port ${port}`)
 })
